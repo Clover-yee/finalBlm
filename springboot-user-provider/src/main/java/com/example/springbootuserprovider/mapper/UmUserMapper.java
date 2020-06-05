f24acb9cdebac74751acac060388b9577d45173e
31701028 计算1701 郑晨鑫 <31701028@stu.zucc.edu.cn>
2020-06-05 10:38:11 +0800

    添加user模块的service层

---
+++ springboot-user-provider/src/main/java/com/example/springbootuserprovider/mapper/UmUserMapper.java
@@ -0,0 +1,7 @@
+package com.example.springbootuserprovider.mapper;
+
+import com.example.api.entity.UmUserEntity;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UmUserMapper extends Mapper<UmUserEntity> {
+}
